f3747ce8c166d5664bc7f6ec1c9dafd6934ec7ea
alirezaSoltani <alireza.soltani1373@gmail.com>
2017-08-23 15:50:11 +0430

    Check In Added - Alireza

---
+++ RayanSport/RayanSport/View/UC_CheckIn.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace RayanSport.View
+{
+    partial class UC_CheckIn
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(51, 41);
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.Size = new System.Drawing.Size(174, 24);
+            this.radTextBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // UC_CheckIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radTextBox1);
+            this.Name = "UC_CheckIn";
+            this.Size = new System.Drawing.Size(395, 339);
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}

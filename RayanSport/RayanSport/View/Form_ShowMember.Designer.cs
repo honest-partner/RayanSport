925fd057ada0606a4418e1a97379e172e60eb272
honest-partner <honest.partner.1993@gmail.com>
2017-08-23 15:54:10 +0430

    Forms added - hosein

---
+++ RayanSport/RayanSport/View/Form_ShowMember.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace RayanSport.View
+{
+    partial class Form_ShowMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form_ShowMember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Name = "Form_ShowMember";
+            this.Text = "Form_ShowMember";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}

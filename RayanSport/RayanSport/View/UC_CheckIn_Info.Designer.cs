d31478e98a8ab2f81907d8acbece8b1fb35d69cb
honest-partner <honest.partner.1993@gmail.com>
2017-08-24 10:19:35 +0430

    models and connection updated - hosein

--- RayanSport/RayanSport/View/UC_CheckIn_Info.Designer.cs
+++ RayanSport/RayanSport/View/UC_CheckIn_Info.Designer.cs
@@ -28,10 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UC_CheckIn_Info));
+            this.pbx_UcCheckInInfoMemberPicture = new System.Windows.Forms.PictureBox();
+            this.txb_UcCheckInInfoMemberId = new System.Windows.Forms.TextBox();
+            this.txb_UcCheckInInfoCommodeNum = new System.Windows.Forms.TextBox();
+            this.txb_UcCheckInInfoMemberName = new System.Windows.Forms.TextBox();
+            this.txb_UcCheckInInfoMemberShipType = new System.Windows.Forms.TextBox();
+            this.txb_UcCheckInInfoMemberRemainingSession = new System.Windows.Forms.TextBox();
+            this.txb_UcCheckInInfoMembershipEndDate = new System.Windows.Forms.TextBox();
+            this.txb_UcCheckInInfoMemberDes = new System.Windows.Forms.TextBox();
+            this.lbl_UcCheckInInfoMemberId = new System.Windows.Forms.Label();
+            this.lbl_UcCheckInInfoCommodeNum = new System.Windows.Forms.Label();
+            this.lbl_UcCheckInInfoMemberName = new System.Windows.Forms.Label();
+            this.lbl_UcCheckInInfoMemberShipType = new System.Windows.Forms.Label();
+            this.lbl_UcCheckInInfoMemberRemainingSession = new System.Windows.Forms.Label();
+            this.lbl_UcCheckInInfoMembershipEndDate = new System.Windows.Forms.Label();
+            this.lbl_UcCheckInInfoMemberDes = new System.Windows.Forms.Label();
+            this.txb_UcCheckInInfoMemberClear = new System.Windows.Forms.Button();
+            this.btn_UcCheckInInfoMemberCheck = new System.Windows.Forms.Button();
+            this.llb_UcCheckInInfoShowMemberDetail = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_UcCheckInInfoMemberPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbx_UcCheckInInfoMemberPicture
+            // 
+            this.pbx_UcCheckInInfoMemberPicture.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx_UcCheckInInfoMemberPicture.BackgroundImage")));
+            this.pbx_UcCheckInInfoMemberPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbx_UcCheckInInfoMemberPicture.Location = new System.Drawing.Point(38, 32);
+            this.pbx_UcCheckInInfoMemberPicture.Name = "pbx_UcCheckInInfoMemberPicture";
+            this.pbx_UcCheckInInfoMemberPicture.Size = new System.Drawing.Size(223, 260);
+            this.pbx_UcCheckInInfoMemberPicture.TabIndex = 0;
+            this.pbx_UcCheckInInfoMemberPicture.TabStop = false;
+            // 
+            // txb_UcCheckInInfoMemberId
+            // 
+            this.txb_UcCheckInInfoMemberId.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_UcCheckInInfoMemberId.Location = new System.Drawing.Point(378, 32);
+            this.txb_UcCheckInInfoMemberId.Name = "txb_UcCheckInInfoMemberId";
+            this.txb_UcCheckInInfoMemberId.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txb_UcCheckInInfoMemberId.Size = new System.Drawing.Size(262, 40);
+            this.txb_UcCheckInInfoMemberId.TabIndex = 1;
+            this.txb_UcCheckInInfoMemberId.Text = "1234";
+            this.txb_UcCheckInInfoMemberId.TextChanged += new System.EventHandler(this.txb_UcCheckInInfoMemberId_TextChanged);
+            this.txb_UcCheckInInfoMemberId.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txb_UcCheckInMemberId_KeyPress);
+            // 
+            // txb_UcCheckInInfoCommodeNum
+            // 
+            this.txb_UcCheckInInfoCommodeNum.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_UcCheckInInfoCommodeNum.Location = new System.Drawing.Point(378, 81);
+            this.txb_UcCheckInInfoCommodeNum.Name = "txb_UcCheckInInfoCommodeNum";
+            this.txb_UcCheckInInfoCommodeNum.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txb_UcCheckInInfoCommodeNum.Size = new System.Drawing.Size(262, 40);
+            this.txb_UcCheckInInfoCommodeNum.TabIndex = 1;
+            this.txb_UcCheckInInfoCommodeNum.Text = "24";
+            // 
+            // txb_UcCheckInInfoMemberName
+            // 
+            this.txb_UcCheckInInfoMemberName.Enabled = false;
+            this.txb_UcCheckInInfoMemberName.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_UcCheckInInfoMemberName.Location = new System.Drawing.Point(378, 127);
+            this.txb_UcCheckInInfoMemberName.Name = "txb_UcCheckInInfoMemberName";
+            this.txb_UcCheckInInfoMemberName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txb_UcCheckInInfoMemberName.Size = new System.Drawing.Size(262, 40);
+            this.txb_UcCheckInInfoMemberName.TabIndex = 1;
+            this.txb_UcCheckInInfoMemberName.Text = "علیرضا سطانی";
+            // 
+            // txb_UcCheckInInfoMemberShipType
+            // 
+            this.txb_UcCheckInInfoMemberShipType.Enabled = false;
+            this.txb_UcCheckInInfoMemberShipType.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_UcCheckInInfoMemberShipType.Location = new System.Drawing.Point(378, 176);
+            this.txb_UcCheckInInfoMemberShipType.Name = "txb_UcCheckInInfoMemberShipType";
+            this.txb_UcCheckInInfoMemberShipType.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txb_UcCheckInInfoMemberShipType.Size = new System.Drawing.Size(262, 40);
+            this.txb_UcCheckInInfoMemberShipType.TabIndex = 1;
+            this.txb_UcCheckInInfoMemberShipType.Text = "ماهانه";
+            // 
+            // txb_UcCheckInInfoMemberRemainingSession
+            // 
+            this.txb_UcCheckInInfoMemberRemainingSession.Enabled = false;
+            this.txb_UcCheckInInfoMemberRemainingSession.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_UcCheckInInfoMemberRemainingSession.Location = new System.Drawing.Point(378, 221);
+            this.txb_UcCheckInInfoMemberRemainingSession.Name = "txb_UcCheckInInfoMemberRemainingSession";
+            this.txb_UcCheckInInfoMemberRemainingSession.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txb_UcCheckInInfoMemberRemainingSession.Size = new System.Drawing.Size(262, 40);
+            this.txb_UcCheckInInfoMemberRemainingSession.TabIndex = 1;
+            this.txb_UcCheckInInfoMemberRemainingSession.Text = "15";
+            // 
+            // txb_UcCheckInInfoMembershipEndDate
+            // 
+            this.txb_UcCheckInInfoMembershipEndDate.Enabled = false;
+            this.txb_UcCheckInInfoMembershipEndDate.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_UcCheckInInfoMembershipEndDate.Location = new System.Drawing.Point(378, 270);
+            this.txb_UcCheckInInfoMembershipEndDate.Name = "txb_UcCheckInInfoMembershipEndDate";
+            this.txb_UcCheckInInfoMembershipEndDate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txb_UcCheckInInfoMembershipEndDate.Size = new System.Drawing.Size(262, 40);
+            this.txb_UcCheckInInfoMembershipEndDate.TabIndex = 1;
+            this.txb_UcCheckInInfoMembershipEndDate.Text = "25/3/95";
+            // 
+            // txb_UcCheckInInfoMemberDes
+            // 
+            this.txb_UcCheckInInfoMemberDes.Enabled = false;
+            this.txb_UcCheckInInfoMemberDes.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_UcCheckInInfoMemberDes.Location = new System.Drawing.Point(378, 323);
+            this.txb_UcCheckInInfoMemberDes.Name = "txb_UcCheckInInfoMemberDes";
+            this.txb_UcCheckInInfoMemberDes.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txb_UcCheckInInfoMemberDes.Size = new System.Drawing.Size(262, 40);
+            this.txb_UcCheckInInfoMemberDes.TabIndex = 1;
+            // 
+            // lbl_UcCheckInInfoMemberId
+            // 
+            this.lbl_UcCheckInInfoMemberId.AutoSize = true;
+            this.lbl_UcCheckInInfoMemberId.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UcCheckInInfoMemberId.Location = new System.Drawing.Point(778, 35);
+            this.lbl_UcCheckInInfoMemberId.Name = "lbl_UcCheckInInfoMemberId";
+            this.lbl_UcCheckInInfoMemberId.Size = new System.Drawing.Size(111, 34);
+            this.lbl_UcCheckInInfoMemberId.TabIndex = 2;
+            this.lbl_UcCheckInInfoMemberId.Text = "کد عضویت";
+            // 
+            // lbl_UcCheckInInfoCommodeNum
+            // 
+            this.lbl_UcCheckInInfoCommodeNum.AutoSize = true;
+            this.lbl_UcCheckInInfoCommodeNum.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UcCheckInInfoCommodeNum.Location = new System.Drawing.Point(782, 84);
+            this.lbl_UcCheckInInfoCommodeNum.Name = "lbl_UcCheckInInfoCommodeNum";
+            this.lbl_UcCheckInInfoCommodeNum.Size = new System.Drawing.Size(107, 34);
+            this.lbl_UcCheckInInfoCommodeNum.TabIndex = 2;
+            this.lbl_UcCheckInInfoCommodeNum.Text = "شماره کمد";
+            // 
+            // lbl_UcCheckInInfoMemberName
+            // 
+            this.lbl_UcCheckInInfoMemberName.AutoSize = true;
+            this.lbl_UcCheckInInfoMemberName.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UcCheckInInfoMemberName.Location = new System.Drawing.Point(718, 130);
+            this.lbl_UcCheckInInfoMemberName.Name = "lbl_UcCheckInInfoMemberName";
+            this.lbl_UcCheckInInfoMemberName.Size = new System.Drawing.Size(171, 34);
+            this.lbl_UcCheckInInfoMemberName.TabIndex = 2;
+            this.lbl_UcCheckInInfoMemberName.Text = "نام و نام خانوادگی";
+            // 
+            // lbl_UcCheckInInfoMemberShipType
+            // 
+            this.lbl_UcCheckInInfoMemberShipType.AutoSize = true;
+            this.lbl_UcCheckInInfoMemberShipType.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UcCheckInInfoMemberShipType.Location = new System.Drawing.Point(777, 179);
+            this.lbl_UcCheckInInfoMemberShipType.Name = "lbl_UcCheckInInfoMemberShipType";
+            this.lbl_UcCheckInInfoMemberShipType.Size = new System.Drawing.Size(112, 34);
+            this.lbl_UcCheckInInfoMemberShipType.TabIndex = 2;
+            this.lbl_UcCheckInInfoMemberShipType.Text = "نوع قرارداد";
+            // 
+            // lbl_UcCheckInInfoMemberRemainingSession
+            // 
+            this.lbl_UcCheckInInfoMemberRemainingSession.AutoSize = true;
+            this.lbl_UcCheckInInfoMemberRemainingSession.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UcCheckInInfoMemberRemainingSession.Location = new System.Drawing.Point(661, 224);
+            this.lbl_UcCheckInInfoMemberRemainingSession.Name = "lbl_UcCheckInInfoMemberRemainingSession";
+            this.lbl_UcCheckInInfoMemberRemainingSession.Size = new System.Drawing.Size(228, 34);
+            this.lbl_UcCheckInInfoMemberRemainingSession.TabIndex = 2;
+            this.lbl_UcCheckInInfoMemberRemainingSession.Text = "تعداد جلسات باقی مانده";
+            // 
+            // lbl_UcCheckInInfoMembershipEndDate
+            // 
+            this.lbl_UcCheckInInfoMembershipEndDate.AutoSize = true;
+            this.lbl_UcCheckInInfoMembershipEndDate.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UcCheckInInfoMembershipEndDate.Location = new System.Drawing.Point(717, 273);
+            this.lbl_UcCheckInInfoMembershipEndDate.Name = "lbl_UcCheckInInfoMembershipEndDate";
+            this.lbl_UcCheckInInfoMembershipEndDate.Size = new System.Drawing.Size(172, 34);
+            this.lbl_UcCheckInInfoMembershipEndDate.TabIndex = 2;
+            this.lbl_UcCheckInInfoMembershipEndDate.Text = "تاریخ پایان قرارداد";
+            // 
+            // lbl_UcCheckInInfoMemberDes
+            // 
+            this.lbl_UcCheckInInfoMemberDes.AutoSize = true;
+            this.lbl_UcCheckInInfoMemberDes.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UcCheckInInfoMemberDes.Location = new System.Drawing.Point(792, 326);
+            this.lbl_UcCheckInInfoMemberDes.Name = "lbl_UcCheckInInfoMemberDes";
+            this.lbl_UcCheckInInfoMemberDes.Size = new System.Drawing.Size(97, 34);
+            this.lbl_UcCheckInInfoMemberDes.TabIndex = 2;
+            this.lbl_UcCheckInInfoMemberDes.Text = "توضیحات";
+            // 
+            // txb_UcCheckInInfoMemberClear
+            // 
+            this.txb_UcCheckInInfoMemberClear.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_UcCheckInInfoMemberClear.Location = new System.Drawing.Point(210, 420);
+            this.txb_UcCheckInInfoMemberClear.Name = "txb_UcCheckInInfoMemberClear";
+            this.txb_UcCheckInInfoMemberClear.Size = new System.Drawing.Size(166, 62);
+            this.txb_UcCheckInInfoMemberClear.TabIndex = 3;
+            this.txb_UcCheckInInfoMemberClear.Text = "پاک کردن";
+            this.txb_UcCheckInInfoMemberClear.UseVisualStyleBackColor = true;
+            this.txb_UcCheckInInfoMemberClear.Click += new System.EventHandler(this.txb_UcCheckInInfoMemberClear_Click);
+            // 
+            // btn_UcCheckInInfoMemberCheck
+            // 
+            this.btn_UcCheckInInfoMemberCheck.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_UcCheckInInfoMemberCheck.Location = new System.Drawing.Point(38, 420);
+            this.btn_UcCheckInInfoMemberCheck.Name = "btn_UcCheckInInfoMemberCheck";
+            this.btn_UcCheckInInfoMemberCheck.Size = new System.Drawing.Size(166, 62);
+            this.btn_UcCheckInInfoMemberCheck.TabIndex = 4;
+            this.btn_UcCheckInInfoMemberCheck.Text = "تایید";
+            this.btn_UcCheckInInfoMemberCheck.UseVisualStyleBackColor = true;
+            this.btn_UcCheckInInfoMemberCheck.Click += new System.EventHandler(this.btn_UcCheckInInfoMemberCheck_Click);
+            // 
+            // llb_UcCheckInInfoShowMemberDetail
+            // 
+            this.llb_UcCheckInInfoShowMemberDetail.AutoSize = true;
+            this.llb_UcCheckInInfoShowMemberDetail.Font = new System.Drawing.Font("B Yekan+", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.llb_UcCheckInInfoShowMemberDetail.Location = new System.Drawing.Point(748, 435);
+            this.llb_UcCheckInInfoShowMemberDetail.Name = "llb_UcCheckInInfoShowMemberDetail";
+            this.llb_UcCheckInInfoShowMemberDetail.Size = new System.Drawing.Size(141, 34);
+            this.llb_UcCheckInInfoShowMemberDetail.TabIndex = 5;
+            this.llb_UcCheckInInfoShowMemberDetail.TabStop = true;
+            this.llb_UcCheckInInfoShowMemberDetail.Text = "نمایش جزئیات";
+            // 
+            // UC_CheckIn_Info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.llb_UcCheckInInfoShowMemberDetail);
+            this.Controls.Add(this.btn_UcCheckInInfoMemberCheck);
+            this.Controls.Add(this.txb_UcCheckInInfoMemberClear);
+            this.Controls.Add(this.lbl_UcCheckInInfoMemberDes);
+            this.Controls.Add(this.lbl_UcCheckInInfoMembershipEndDate);
+            this.Controls.Add(this.lbl_UcCheckInInfoMemberRemainingSession);
+            this.Controls.Add(this.lbl_UcCheckInInfoMemberShipType);
+            this.Controls.Add(this.lbl_UcCheckInInfoMemberName);
+            this.Controls.Add(this.lbl_UcCheckInInfoCommodeNum);
+            this.Controls.Add(this.lbl_UcCheckInInfoMemberId);
+            this.Controls.Add(this.txb_UcCheckInInfoMemberDes);
+            this.Controls.Add(this.txb_UcCheckInInfoMembershipEndDate);
+            this.Controls.Add(this.txb_UcCheckInInfoMemberRemainingSession);
+            this.Controls.Add(this.txb_UcCheckInInfoMemberShipType);
+            this.Controls.Add(this.txb_UcCheckInInfoMemberName);
+            this.Controls.Add(this.txb_UcCheckInInfoCommodeNum);
+            this.Controls.Add(this.txb_UcCheckInInfoMemberId);
+            this.Controls.Add(this.pbx_UcCheckInInfoMemberPicture);
+            this.Name = "UC_CheckIn_Info";
+            this.Size = new System.Drawing.Size(910, 507);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_UcCheckInInfoMemberPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbx_UcCheckInInfoMemberPicture;
+        private System.Windows.Forms.TextBox txb_UcCheckInInfoMemberId;
+        private System.Windows.Forms.TextBox txb_UcCheckInInfoCommodeNum;
+        private System.Windows.Forms.TextBox txb_UcCheckInInfoMemberName;
+        private System.Windows.Forms.TextBox txb_UcCheckInInfoMemberShipType;
+        private System.Windows.Forms.TextBox txb_UcCheckInInfoMemberRemainingSession;
+        private System.Windows.Forms.TextBox txb_UcCheckInInfoMembershipEndDate;
+        private System.Windows.Forms.TextBox txb_UcCheckInInfoMemberDes;
+        private System.Windows.Forms.Label lbl_UcCheckInInfoMemberId;
+        private System.Windows.Forms.Label lbl_UcCheckInInfoCommodeNum;
+        private System.Windows.Forms.Label lbl_UcCheckInInfoMemberName;
+        private System.Windows.Forms.Label lbl_UcCheckInInfoMemberShipType;
+        private System.Windows.Forms.Label lbl_UcCheckInInfoMemberRemainingSession;
+        private System.Windows.Forms.Label lbl_UcCheckInInfoMembershipEndDate;
+        private System.Windows.Forms.Label lbl_UcCheckInInfoMemberDes;
+        private System.Windows.Forms.Button txb_UcCheckInInfoMemberClear;
+        private System.Windows.Forms.Button btn_UcCheckInInfoMemberCheck;
+        private System.Windows.Forms.LinkLabel llb_UcCheckInInfoShowMemberDetail;
     }
 }
--- RayanSport/RayanSport/View/UC_CheckIn_Info.cs
+++ RayanSport/RayanSport/View/UC_CheckIn_Info.cs
@@ -12,9 +12,99 @@ namespace RayanSport.View
 {
     public partial class UC_CheckIn_Info : UserControl
     {
+        //id for people that are not member but they came gym
+        int not_member_id;
         public UC_CheckIn_Info()
         {
             InitializeComponent();
+        }
+
+        private void txb_UcCheckInMemberId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //check that input is numeric
+            e.Handled = !(Char.IsNumber(e.KeyChar) || e.KeyChar == 8);
+
+        }
+
+        private void btn_UcCheckInMemberCheck_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txb_UcCheckInMemberId_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            //check if enter key is entered
+            if (e.KeyCode == Keys.Enter) {
+                
+                
+            }
+        }
+        //check id is corect.if corect is it memeber or not
+        private void CheckId(String member_id)
+        {
+            int member_id_int;
+            bool access = Int32.TryParse(member_id, out member_id_int);
+            if (!access)
+            {
+                MessageBox.Show("لطفا کد عضویت را بصورت صحیح وارد کنید");
+            }
+            else {
+                if (member_id_int == not_member_id)
+                    NotMemberEntered();
+                else
+                    CheckMember();
+            }
+            
+        }
+        //do task if entered id is member or not
+        private void CheckMember()
+        {
+            
+        }
+        //do task if entered id is for not memebers
+        private void NotMemberEntered()
+        {
+            
+        }
+
+        private void txb_UcCheckInInfoMemberClear_Click(object sender, EventArgs e)
+        {
+            ClearTexts();
+            
+        }
+
+        private void ClearTexts()
+        {
+            foreach (var txt in this.Controls)
+            {
+                if (txt is TextBox)
+                    ((TextBox)txt).Clear();
+            }
+        }
+
+        private void btn_UcCheckInInfoMemberCheck_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txb_UcCheckInInfoMemberId_TextChanged(object sender, EventArgs e)
+        {
+            /*rayan_sportDataSet.membershipDataTable dataTable = new rayan_sportDataSet.membershipDataTable();
+            rayan_sportDataSetTableAdapters.membershipTableAdapter adapter = new rayan_sportDataSetTableAdapters.membershipTableAdapter();
+            int rows =adapter.FillByCheckMemberIsExist(dataTable, Convert.ToInt64(txb_UcCheckInInfoMemberId.Text));
+            if (rows == 1)
+            {
+                txb_UcCheckInInfoMemberName.Text = dataTable[0].membership_memberName;
+                txb_UcCheckInInfoMemberShipType.Text = dataTable[0].membership_type;
+                txb_UcCheckInInfoMemberRemainingSession.Text = dataTable[0].membership_remainingSession + "";
+                txb_UcCheckInInfoMembershipEndDate.Text = dataTable[0].membership_endDate;
+                //txb_UcCheckInInfoMemberDes.Text = dataTable[0].
+            }
+            else {
+                MessageBox.Show("نام کاربری و یا رمز عبور وارد شده صحیح نمیباشد");
+                ClearTexts();
+            }*/
         }
     }
 }

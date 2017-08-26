65d1dce10186c4b1747fa530cc7e7686ef5a68fa
alirezaSoltani <alireza.soltani1373@gmail.com>
2017-08-26 09:58:01 +0430

    a

---
+++ RayanSport/RayanSport/rayan_sportDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace RayanSport
+{
+
+
+    partial class rayan_sportDataSet
+    {
+        partial class membershipDataTable
+        {
+        }
+    }
+}
+
+namespace RayanSport.rayan_sportDataSetTableAdapters {
+    
+    
+    public partial class membershipTableAdapter {
+    }
+}
